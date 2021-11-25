f65a7e05670f05cf655aa8d7b6ac6a28d148149b
FlutLab.io <>
2021-11-25 04:38:56 +0000

    Autosave

--- lib/home.dart
+++ lib/home.dart
@@ -11,31 +11,33 @@ class _State extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            height: 200,
-          ),
-          Container(
-            height: 500,
-            padding: EdgeInsets.all(10),
-            child: FutureBuilder(
-              future: fetchWpPost(),
-              builder: (BuildContext context, AsyncSnapshot snapshot) {
-                if (snapshot.hasData) {
-                  return ListView.builder(
-                    itemCount: snapshot.data.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      Map myposts = snapshot.data[index];
-                      return PostTile(href: myposts['_links']["wp:featuredmedia"][0]['href'], title: myposts['title']['rendered'].replaceAll('#038;', ""), desc: myposts['excerpt']['rendered'], content: myposts['content']['rendered']);
-                    },
-                  );
-                }
-                return CircularProgressIndicator();
-              },
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 200,
             ),
-          ),
-        ],
+            Container(
+              height: 500,
+              padding: EdgeInsets.all(10),
+              child: FutureBuilder(
+                future: fetchWpPost(),
+                builder: (BuildContext context, AsyncSnapshot snapshot) {
+                  if (snapshot.hasData) {
+                    return ListView.builder(
+                      itemCount: snapshot.data.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        Map myposts = snapshot.data[index];
+                        return PostTile(href: myposts['_links']["wp:featuredmedia"][0]['href'], title: myposts['title']['rendered'].replaceAll('#038;', ""), desc: myposts['excerpt']['rendered'], content: myposts['content']['rendered']);
+                      },
+                    );
+                  }
+                  return CircularProgressIndicator();
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
