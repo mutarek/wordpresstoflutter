7a90e0a8a21bbfc4f857ec2bba4bb84ef634e388
FlutLab.io <>
2021-11-25 12:48:07 +0000

    Autosave

--- lib/home.dart
+++ lib/home.dart
@@ -99,7 +99,7 @@ class PostState extends State<PostTile> {
                 builder: (BuildContext context, AsyncSnapshot snapshot) {
                   if (snapshot.hasData) {
                     imgurl = snapshot.data["guid"]["rendered"];
-                    return ClipRRect(borderRadius: BorderRadius.circular(10), child: Image.network(snapshot.data["guid"]["rendered"]));
+                    return ClipRRect(borderRadius: BorderRadius.circular(10.0), child: Image.network(snapshot.data["guid"]["rendered"]));
                   }
                   return CircularProgressIndicator();
                 },
