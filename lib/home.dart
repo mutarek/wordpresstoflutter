f818c12dac253489c9ba28a690371b1a11cc9419
FlutLab.io <>
2021-11-25 07:49:23 +0000

    Autosave

--- lib/home.dart
+++ lib/home.dart
@@ -11,28 +11,22 @@ class _State extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              padding: EdgeInsets.all(10),
-              child: FutureBuilder(
-                future: fetchWpPost(),
-                builder: (BuildContext context, AsyncSnapshot snapshot) {
-                  if (snapshot.hasData) {
-                    return ListView.builder(
-                      itemCount: snapshot.data.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        Map myposts = snapshot.data[index];
-                        return PostTile(href: myposts['_links']["wp:featuredmedia"][0]['href'], title: myposts['title']['rendered'].replaceAll('#038;', ""), desc: myposts['excerpt']['rendered'], content: myposts['content']['rendered']);
-                      },
-                    );
-                  }
-                  return CircularProgressIndicator();
+      body: Container(
+        padding: EdgeInsets.all(10),
+        child: FutureBuilder(
+          future: fetchWpPost(),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (snapshot.hasData) {
+              return ListView.builder(
+                itemCount: snapshot.data.length,
+                itemBuilder: (BuildContext context, int index) {
+                  Map myposts = snapshot.data[index];
+                  return PostTile(href: myposts['_links']["wp:featuredmedia"][0]['href'], title: myposts['title']['rendered'].replaceAll('#038;', ""), desc: myposts['excerpt']['rendered'], content: myposts['content']['rendered']);
                 },
-              ),
-            ),
-          ],
+              );
+            }
+            return CircularProgressIndicator();
+          },
         ),
       ),
     );
