210e122ea24404b3da1ad67f05aaecf0bc3c9012
FlutLab.io <>
2021-11-29 04:49:39 +0000

    Autosave

--- lib/widgets/customDrawer.dart
+++ lib/widgets/customDrawer.dart
@@ -1,2 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+Widget customDrawer() {
+  return SingleChildScrollView(
+    child: Padding(
+      padding: EdgeInsets.all(20.0),
+      child: Column(
+        children: [],
+      ),
+    ),
+  );
+}
