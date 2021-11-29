d8b21c14ca6a368a0b713d5ccad4e237171ffb23
FlutLab.io <>
2021-11-29 06:02:27 +0000

    Autosave

--- lib/widgets/customDrawer.dart
+++ lib/widgets/customDrawer.dart
@@ -15,13 +15,20 @@ Widget customDrawer() {
             child: Container(
               height: 150,
               width: double.infinity,
-              child: CircleAvatar(
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 1,
+                   child: CircleAvatar(
                 backgroundColor: Colors.teal,
                 radius: 50,
                 child: ClipRRect(
                   borderRadius: BorderRadius.circular(99.0),
                   child: Image.network('https://seidnacional.com/portaldoaluno/wp-content/uploads/learn-press-profile/4/172522ec1028ab781d9dfd17eaca4427.jpg'),
                 ),
+              ),,
+                  )
+                ],
               ),
             ),
           )
