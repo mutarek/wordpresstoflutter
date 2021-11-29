7f43584cba40f68f5c63023e86e7d64d73569a44
FlutLab.io <>
2021-11-29 06:12:27 +0000

    Autosave

--- lib/widgets/customDrawer.dart
+++ lib/widgets/customDrawer.dart
@@ -46,7 +46,13 @@ Widget customDrawer() {
               ),
             ),
           ),
-          Divider()
+          Divider(
+            thickness: 5, // thickness of the line
+            indent: 20, // empty space to the leading edge of divider.
+            endIndent: 20, // empty space to the trailing edge of the divider.
+            color: Colors.black, // The color to use when painting the line.
+            height: 20, // The divider's height extent.
+          ),
         ],
       ),
     ),
