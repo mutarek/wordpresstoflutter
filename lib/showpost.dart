e0970ebc96bb4c8cf5cb82bfc8ca7418b4168def
FlutLab.io <>
2021-12-09 04:12:10 +0000

    Autosave

--- lib/showpost.dart
+++ lib/showpost.dart
@@ -27,7 +27,7 @@ class ShowState extends State<ShowPost> {
           builder: (BuildContext context, AsyncSnapshot snapshot) {
             if (snapshot.hasData) {
               return ListView.builder(
-                itemCount: snapshot.data.length,
+                itemCount: snapshot.data.take(5).length,
                 itemBuilder: (BuildContext context, int index) {
                   Map myposts = snapshot.data[index];
                   return PostTile(href: myposts['_links']["wp:featuredmedia"][0]['href'], title: myposts['title']['rendered'].replaceAll('#038;', ""), desc: myposts['excerpt']['rendered'], content: myposts['content']['rendered']);
